946dc06e89cc2f13b6814d8e69c20085ddf178ce
adyajudha <adityayudhapratama127@outlook.com>
2024-05-20 14:43:47 +0700

    Perbaiki Statement

--- statement_prepared.php
+++ statement_prepared.php
@@ -3,25 +3,33 @@
         $dbh = new PDO("mysql:host=localhost;dbname=pegawai", "root", "");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // Menambahkan data pada tabel Jabatan
-        $st = $dbh->prepare("INSERT INTO pegawai VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $st = $dbh->prepare("INSERT INTO pegawai VALUES (:id, :nama, :jenis_kelamin, :tgl_lahir, :id_jabatan, :foto, :ket)");
 
-        $st->bindParam(1, $id);
-        $st->bindParam(2, $nama);
-        $st->bindParam(3, $jenis_kelamin);
-        $st->bindParam(4, $tgl_lahir);
-        $st->bindParam(5, $id_jabatan);
-        $st->bindParam(6, $foto);
-        $st->bindParam(7, $ket);
+        $st->bindParam(':id', $id);
+        $st->bindParam(':nama', $nama);
+        $st->bindParam(':jenis_kelamin', $jenis_kelamin);
+        $st->bindParam(':tgl_lahir', $tgl_lahir);
+        $st->bindParam(':id_jabatan', $id_jabatan);
+        $st->bindParam(':foto', $foto);
+        $st->bindParam(':ket', $ket);
 
         $id = 'NULL';
         $nama = 'Aditya Yudha Pratama';
         $jenis_kelamin = 'L';
-        $tgl_lahir = '1990-07-08';
+        $tgl_lahir = '2001-05-19';
         $id_jabatan = 1;
         $foto = 'yuda.jpg';
         $ket = 'Direktur Utama';
 
-        $st->execute();
+        $st->execute(array(
+            ':id' => $id,
+            ':nama' => $nama,
+            ':jenis_kelamin' => $jenis_kelamin,
+            ':tgl_lahir' => $tgl_lahir,
+            'id_jabatan' => $id_jabatan,
+            ':foto' => $foto,
+            ':ket' => $ket
+        ));
 
         echo $st->rowCount()." berhasil ditambahkan.";
 
